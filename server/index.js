fc5cf8c3de3458a30bcbc465a9ff7947141b4697
adamhe(何方舟) <adamhe@tencent.com>
2018-12-13 16:43:40 +0800

    faet: init

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+    ctx.body = 'hello world';
+});
+
+app.listen(3000);
---
+++ src/js/entry/index.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import ReactDom from 'react-dom';
+
+function list() {
+    const [] = useState();
+}
