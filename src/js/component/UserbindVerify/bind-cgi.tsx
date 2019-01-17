f7128280405f7e9282e8843d22a41cc3bcc8a050
eczn <1975845422@qq.com>
2019-01-17 21:03:43 +0800

    ~ 添加用户审核页面

--- src/js/component/UserbindVerify/bind-cgi.tsx
+++ src/js/component/UserbindVerify/bind-cgi.tsx
@@ -1,5 +1,5 @@
 import { fromBadjs } from "../../common/api"; 
-import { UserInfoWithOpenid } from "../QQLogin";
+import { UserInfoWithOpenid, VerifyStateValue } from "../QQLogin";
 
 // export function isUserInfoResp(resp: LoginBySuccess): resp is UserInfo {
 //     if ((resp as OpenidInfo).openid) {
@@ -8,10 +8,36 @@ import { UserInfoWithOpenid } from "../QQLogin";
 //         return true; 
 //     }
 // }
-
+export type VerifyResult = {
+    code: number, 
+    message: string
+}
 
 export function getVerifyUsers() {
     // 更新 Session 
     return fromBadjs.get<UserInfoWithOpenid[]>('/api/verify/users');
 }
 
+export function trustAnUser(loginName: string) {
+    return fromBadjs.post<VerifyResult>('/api/verify/trust_him', {
+        loginName
+    });
+}
+
+export function verifyAnUser(user: UserInfoWithOpenid) {
+    console.log(user); 
+
+    const goNext = confirm(`给用户 ${ user.loginName } 通过审核吗？`); 
+
+    if (goNext) {
+        return trustAnUser(user.loginName).then(res => {
+            if (res.code === 0) {
+                return Promise.resolve(); 
+            } else {
+                return Promise.reject(res); 
+            }
+        })
+    } else {
+        return Promise.reject(false); 
+    }
+}
--- src/js/component/UserbindVerify/index.tsx
+++ src/js/component/UserbindVerify/index.tsx
@@ -1,31 +1,107 @@
 import * as React from "react"; 
 
-import { getVerifyUsers } from "./bind-cgi";
+import { Table, Divider } from "antd"; 
+import { getVerifyUsers, verifyAnUser } from "./bind-cgi";
 import { loginCtx, UserInfo, UserInfoWithOpenid } from "../QQLogin";
 
-function UB(props: { userInfo: UserInfo }) {
-    const [userList, setUserList] = React.useState([] as UserInfoWithOpenid[])
 
-    React.useEffect(() => {
+const dataSource = [{
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号'
+  }, {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号'
+  }];
+  
+//   const columns = [{
+//     title: '姓名',
+//     dataIndex: 'name',
+//     key: 'name',
+//   }, {
+//     title: '年龄',
+//     dataIndex: 'age',
+//     key: 'age',
+//   }, {
+//     title: '住址',
+//     dataIndex: 'address',
+//     key: 'address',
+//   }];
+  
+
+function UB(props: { userInfo: UserInfo }) {
+    const [userList, setUserList] = React.useState([] as UserInfoWithOpenid[]); 
+
+    const initLoad = () => {
         getVerifyUsers().then(newUserList => {
             console.log('getVerifyUsers', newUserList); 
 
             setUserList(newUserList); 
         })
-    }, []); 
+    }
 
-    const list = userList.map((user, idx) => {
-        return (
-            <div className="bind-user" key={ idx }>
-                { idx + 1 }: 名字: { user.loginName }; 想绑定的 userId { user.openid }; 当前审核状态 { ['未提交审核', '审核中', '已绑定'][user.verify_state] }
-            </div>
-        )
-    });
+    React.useEffect(initLoad, []); 
+
+    const intro = (
+        <div style={{ marginTop: '1.5em', paddingLeft: '16px' }}>
+            共计 { userList.length } 个用户，有 { userList.filter(e => +e.verify_state === 1).length } 个待审核 <br />
+            
+            审核状态分三种 0 1 2，分别对应: <br />
+            
+            老用户未申请
+
+            <Divider type="vertical" />
+            申请审核中
+
+            <Divider type="vertical" />
+            审核通过
+        </div>
+    ); 
 
     return (
         <div>
-            <h1>列表</h1>
-            { list }
+            <h1 style={{ paddingLeft: '14px' }}>用户绑定审核</h1>
+            <Table dataSource={ userList } pagination={ false }  columns={[{
+                title: '用户 RTX', key: 'loginName',
+                dataIndex: 'loginName'
+            }, {
+                title: '当前审核状态', key: 'verify_state',
+                dataIndex: 'verify_state',
+                render: verify_state => 
+                    <span>{ ['老用户暂未登陆 ⌛️', '待审核 ⚖️', '已审核 ✅'][parseInt(verify_state, 10)] }</span>
+            }, {
+                title: '操作', 
+                key: 'action', 
+                render: (text, record) => {
+                    if (+record.verify_state === 1) {
+                    return <a href="javascript:;" onClick={
+                        () => verifyAnUser(record).then(() => {
+                            // 审核成功
+                            initLoad(); 
+                        }).catch(err => {
+                            if (err) {
+                                alert('错误，原因：' + JSON.stringify(err)); 
+                            }
+                        })
+                    }>审核通过?</a>
+                    } else if (+record.verify_state === 0) {
+                        return <span>- 无操作</span>
+                    } else {
+                        return <span>已审核</span>
+                    }
+
+                    // <span>
+                    //   <a href="javascript:;">{ record.verify_state }</a>
+                    //   <Divider type="vertical" />
+                    //   <a href="javascript:;">Delete</a>
+                    // </span>
+                }
+            }]} />
+
+            { intro }
         </div>
     )
 }
