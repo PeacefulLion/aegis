bf804f9536aec45dc4fc13814c7b48115440909a
andyzou <andyzou@tencent.com>
2019-05-29 17:54:13 +0800

    使用parcel处理md文件

--- src/js/page/changelog/index.tsx
+++ src/js/page/changelog/index.tsx
@@ -1,22 +1,12 @@
 import * as React from 'react';
-import Markdown from 'markdown-to-jsx';
 import './index.less';
-import Axios from 'axios';
 const { useState, useEffect } = React;
-
+import mdContent from '../../../../CHANGELOG.md';
 export default function ChangeLog() {
-    let [changelog, setChangelog] = useState('');
-    useEffect(() => {
-        // 不直接写async函数，否则会出现warnning
-        const fetchData = async () => {
-          const result = await Axios.get('https://raw.githubusercontent.com/iv-web/aegis/master/CHANGELOG.md');
-          setChangelog(result.data);
-        };
-        fetchData();
-      }, []);
+
     return (
         <div>
-            <Markdown>{changelog}</Markdown>
+            <div dangerouslySetInnerHTML={{__html: mdContent}} />
         </div>
     )
 }
